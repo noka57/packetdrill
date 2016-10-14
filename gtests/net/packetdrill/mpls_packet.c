90952a18c97541a4ba2dc20fd15c6c212f01b9be
Noka <noka.tang@moxa.com>
2016-10-14 14:05:54 +0800

    for ecos

--- gtests/net/packetdrill/mpls_packet.c
+++ gtests/net/packetdrill/mpls_packet.c
@@ -27,21 +27,42 @@
 #include "mpls.h"
 
 int new_mpls_stack_entry(s64 label, s64 traffic_class,
-			 bool is_stack_bottom, s64 ttl,
-			 struct mpls *mpls, char **error)
+                         bool is_stack_bottom, s64 ttl,
+                         struct mpls *mpls, char **error)
 {
-	if ((label < 0) || (label >= (1<<20))) {
+	if ((label < 0) || (label >= (1 << 20)))
+	{
+#ifdef ECOS
+		int len = strlen("MPLS label out of range for 20 bits");
+		*error = malloc(len);
+		snprintf(*error, len, "MPLS label out of range for 20 bits");
+#else
 		asprintf(error, "MPLS label out of range for 20 bits");
+#endif
 		return STATUS_ERR;
 	}
 
-	if ((traffic_class < 0) || (traffic_class >= (1<<3))) {
+	if ((traffic_class < 0) || (traffic_class >= (1 << 3)))
+	{
+#ifdef ECOS
+		int len = strlen("MPLS traffic_class out of range for 3 bits");
+		*error = malloc(len);
+		snprintf(*error, len, "MPLS traffic_class out of range for 3 bits");
+#else
 		asprintf(error, "MPLS traffic_class out of range for 3 bits");
+#endif
 		return STATUS_ERR;
 	}
 
-	if ((ttl < 0) || (ttl >= (1<<8))) {
+	if ((ttl < 0) || (ttl >= (1 << 8)))
+	{
+#ifdef ECOS
+		int len = strlen("MPLS ttl out of range for 8 bits");
+		*error = malloc(len);
+		snprintf(*error, len, "MPLS ttl out of range for 8 bits");
+#else
 		asprintf(error, "MPLS ttl out of range for 8 bits");
+#endif
 		return STATUS_ERR;
 	}
 
@@ -50,14 +71,21 @@ int new_mpls_stack_entry(s64 label, s64 traffic_class,
 }
 
 int mpls_header_append(struct packet *packet, struct mpls_stack *mpls_stack,
-		       char **error)
+                       char **error)
 {
 	struct header *header;
 	int mpls_bytes = mpls_stack->length * sizeof(struct mpls);
 
 	header = packet_append_header(packet, HEADER_MPLS, mpls_bytes);
-	if (header == NULL) {
+	if (header == NULL)
+	{
+#ifdef ECOS
+		int len = strlen("too many headers");
+		*error = malloc(len);
+		snprintf(*error, len, "too many headers");
+#else
 		asprintf(error, "too many headers");
+#endif
 		return STATUS_ERR;
 	}
 
@@ -67,7 +95,7 @@ int mpls_header_append(struct packet *packet, struct mpls_stack *mpls_stack,
 }
 
 int mpls_header_finish(struct packet *packet,
-		      struct header *header, struct header *next_inner)
+                       struct header *header, struct header *next_inner)
 {
 	int mpls_bytes = header->header_bytes + next_inner->total_bytes;
 
